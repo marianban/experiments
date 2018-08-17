396fbd903a011b8ae20d539452ac3d6c82ee5bf6
Marian Ban <marian.ban@gmail.com>
2018-08-17 20:42:35 +0200

    client app finished

--- serverless-tutorial/notes-app-client/src/containers/Home.js
+++ serverless-tutorial/notes-app-client/src/containers/Home.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import { PageHeader, ListGroup, ListGroupItem } from "react-bootstrap";
 import { API } from "aws-amplify";
+import { Link } from "react-router-dom";
 import "./Home.css";
 
 export default class Home extends Component {
@@ -66,6 +67,14 @@ export default class Home extends Component {
       <div className="lander">
         <h1>Scratch</h1>
         <p>A simple note taking app</p>
+        <div>
+          <Link to="/login" className="btn btn-info btn-lg">
+            Login
+          </Link>
+          <Link to="/signup" className="btn btn-success btn-lg">
+            Signup
+          </Link>
+        </div>
       </div>
     );
   }
