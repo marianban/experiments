7adcf00bf8f007596b6351ca48c8e11213b9dc9c
Marian Ban <marian.ban@gmail.com>
2019-04-09 21:18:52 +0200

    amplify notetaker

--- amplify-notetaker/src/App.js
+++ amplify-notetaker/src/App.js
@@ -1,31 +1,164 @@
 import React, { Component } from 'react';
 import { withAuthenticator } from 'aws-amplify-react';
+import { API, graphqlOperation } from 'aws-amplify';
+import { createNote, deleteNote, updateNote } from './graphql/mutations';
+import { listNotes } from './graphql/queries';
+import {
+  onCreateNote,
+  onDeleteNote,
+  onUpdateNote
+} from './graphql/subscriptions';
 
 class App extends Component {
   state = {
-    notes: [
-      {
-        id: 1,
-        note: 'Hello world'
-      }
-    ]
+    id: '',
+    note: '',
+    notes: []
   };
 
-  render() {
+  async componentDidMount() {
+    this.getNotes();
+    this.createNoteListener = API.graphql(
+      graphqlOperation(onCreateNote)
+    ).subscribe({
+      next: noteData => {
+        const newNote = noteData.value.data.onCreateNote;
+        const prevNotes = this.state.notes.filter(
+          note => note.id !== newNote.id
+        );
+        const updatedNotes = [...prevNotes, newNote];
+        this.setState({
+          notes: updatedNotes
+        });
+      }
+    });
+    this.deleteNoteListener = API.graphql(
+      graphqlOperation(onDeleteNote)
+    ).subscribe({
+      next: noteData => {
+        const deletedNote = noteData.value.data.onDeleteNote;
+        const updatedNotes = this.state.notes.filter(
+          note => note.id !== deletedNote.id
+        );
+        this.setState({
+          notes: updatedNotes
+        });
+      }
+    });
+    this.updateNoteListener = API.graphql(
+      graphqlOperation(onUpdateNote)
+    ).subscribe({
+      next: noteData => {
+        const { notes } = this.state;
+        const updatedNote = noteData.value.data.onUpdateNote;
+        const index = notes.findIndex(note => note.id === updatedNote.id);
+        const updatedNotes = [
+          ...notes.slice(0, index),
+          updatedNote,
+          ...notes.slice(index + 1)
+        ];
+        this.setState({
+          notes: updatedNotes,
+          note: '',
+          id: ''
+        });
+      }
+    });
+  }
+
+  componentWillUnmount() {
+    this.createNoteListener.unsubscribe();
+    this.deleteNoteListener.unsubscribe();
+    this.updateNoteListener.unsubscribe();
+  }
+
+  getNotes = async () => {
+    const result = await API.graphql(graphqlOperation(listNotes));
+    this.setState({ notes: result.data.listNotes.items });
+  };
+
+  handleChangeNote = event => {
+    this.setState({ note: event.target.value });
+  };
+
+  hasExistingNote = () => {
+    const { notes, id } = this.state;
+    if (id) {
+      const isNote = notes.findIndex(note => note.id === id) > -1;
+      return isNote;
+    }
+    return false;
+  };
+
+  handleAddNote = async event => {
+    const { note } = this.state;
+    event.preventDefault();
+    if (this.hasExistingNote()) {
+      this.handleUpdateNote();
+    } else {
+      const input = {
+        note
+      };
+      await API.graphql(graphqlOperation(createNote, { input }));
+      this.setState({
+        note: ''
+      });
+    }
+  };
+
+  handleUpdateNote = async () => {
+    const { id, note } = this.state;
+    const input = { id, note };
+    await API.graphql(graphqlOperation(updateNote, { input }));
+  };
+
+  handleDeleteNote = async noteId => {
     const { notes } = this.state;
+    const input = { id: noteId };
+    const result = await API.graphql(graphqlOperation(deleteNote, { input }));
+    const deletedNoteId = result.data.deleteNote.id;
+    const updatedNotes = notes.filter(note => note.id !== deletedNoteId);
+    this.setState({ notes: updatedNotes });
+  };
+
+  handleSetNote = ({ note, id }) =>
+    this.setState({
+      note,
+      id
+    });
+
+  render() {
+    const { notes, note, id } = this.state;
 
     return (
       <div className="flex flex-column items-center justify-center pa3 bg-washed-red">
         <h1 className="code f2-l">Notetaker</h1>
-        <form className="mb3">
-          <input type="text" className="pa2 f4" placeholder="Write your note" />
+        <form className="mb3" onSubmit={this.handleAddNote}>
+          <input
+            type="text"
+            className="pa2 f4"
+            placeholder="Write your note"
+            value={note}
+            onChange={this.handleChangeNote}
+          />
+          <button className="pa2 f4" type="submit">
+            {id ? 'Update Note' : 'Add Note'}
+          </button>
         </form>
 
         <div>
           {notes.map(item => (
             <div key={item.id} className="flex items-center">
-              <li className="list pa2 f3">{item.note}</li>
-              <button className="bg-transparent bn f4">
+              <li
+                className="list pa2 f3"
+                onClick={() => this.handleSetNote(item)}
+              >
+                {item.note}
+              </li>
+              <button
+                className="bg-transparent bn f4"
+                onClick={() => this.handleDeleteNote(item.id)}
+              >
                 <span>&times;</span>
               </button>
             </div>
@@ -37,6 +170,10 @@ class App extends Component {
 }
 
 // marian.ban@hotmail.com
+// Marian$11
+
+// marian.ban@gmail.com
+// marian.ban
 // Marian$11
 
 export default withAuthenticator(App, { includeGreetings: true });
--- amplify-notetaker/src/index.js
+++ amplify-notetaker/src/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import App from './AppHooks';
 import * as serviceWorker from './serviceWorker';
 import Amplify from 'aws-amplify';
 import aws_exports from './aws-exports';
